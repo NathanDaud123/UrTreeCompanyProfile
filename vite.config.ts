bf777974f4b45dadb94878105fec9af3f98d24c3
Nathan <nathandaud123@gmail.com>
2025-10-27 02:16:37 +0700

    Fix base path configuration for production build

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
 export default defineConfig({
-  base: '/',
+  base: process.env.NODE_ENV === 'production' ? '/UrTreeCompanyProfile/' : '/',
   plugins: [react()],
   resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
